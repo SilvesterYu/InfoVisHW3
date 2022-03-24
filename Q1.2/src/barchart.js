67e2427733d2d65340126b67e23f390cead10c35
Silvester Yu <ly1164@nyu.edu>
2022-03-24 11:34:51 +0400

    ver.2

--- Q1.2/src/barchart.js
+++ Q1.2/src/barchart.js
@@ -8,18 +8,8 @@ import { Bars } from './bars';
 export function BarChart (props) {
 
     // -- Task 1.2 -- //
-    const{data, offsetX, offsetY, height, width} = props;
-
-    console.log("/-----------/");
-    console.log(data);
-    console.log(data.start);
-    console.log("//----------//");
-    
+    const{data, offsetX, offsetY, height, width} = props;    
     const xScale = Scales.band(data.map(d => `${d.station}`), 0, width);
-    console.log("+++++");
-    console.log(data.map(d => `${d.station}`));
-    console.log(xScale.domain());
-    console.log("++++++");
     const yScale = Scales.linear(0, max(data, d => d.start), height, 0);
     return <g transform={`translate(${offsetX}, ${offsetY})`}>
         <Bars data={data} xScale={xScale} yScale={yScale} height={height}/>
