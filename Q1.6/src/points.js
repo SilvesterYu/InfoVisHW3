0d57eb9644b37855a01e646abffbef2ecc0ef9a8
Silvester Yu <ly1164@nyu.edu>
2022-03-26 12:19:24 +0400

    ver.1

---
+++ Q1.6/src/points.js
@@ -0,0 +1,47 @@
+
+import React from "react";
+import { Tooltip } from "./tooltip";
+
+export function Points(props) {
+
+    const {data, xScale, yScale, height, width, selectedStation, mouseEnter, mouseOut} = props;
+
+    //complete the getColor and getRadius when you are asked to
+    const getColor = (selectedStation, d) => {
+        if (selectedStation === d){
+            return "red";
+        } else {
+            return "steelblue";
+        }
+    }
+    const getRadius = (selectedStation, d) => {
+        if (selectedStation === d) {
+            return 10;
+        } else {
+            return 5;
+        }
+    }
+
+    if (selectedStation === null) {
+        return <g>
+            {data.map(d => {
+                return <circle key={d.station} cx={xScale(d.tripdurationS)} 
+                cy={yScale(d.tripdurationE)} r={5} fill={"steelblue"} stroke={"black"}
+                onMouseEnter={() => mouseEnter(d)} onMouseOut={mouseOut}/>
+            })}
+        </g>
+    } else {
+        return <g>
+            {data.map(d => {
+                return <circle key={d.station} cx={xScale(d.tripdurationS)} 
+                cy={yScale(d.tripdurationE)} r={5} fill={"steelblue"} stroke={"black"}
+                onMouseEnter={() => mouseEnter(d)} onMouseOut={mouseOut}/>
+            })}
+            <rect x={0} y={0} width={width} height={height} fill={"yellow"} opacity={0.5}/>
+            {data.filter(d => d.station === selectedStation.station).map( d => {
+                return <circle key={d.station} cx={xScale(d.tripdurationS)} 
+                cy={yScale(d.tripdurationE)} r={getRadius(selectedStation, d)} fill={getColor(selectedStation, d)} stroke={"black"}
+                onMouseEnter={()=>{mouseEnter(d)}} onMouseOut={mouseOut}/>
+            })}
+        </g>}
+}
