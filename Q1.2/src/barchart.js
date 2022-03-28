a469783bd74e6906b3c683818c23d9912ea8f617
Silvester Yu <ly1164@nyu.edu>
2022-03-28 09:54:35 +0400

    ver.4

--- Q1.2/src/barchart.js
+++ Q1.2/src/barchart.js
@@ -7,9 +7,9 @@ import { Bars } from './bars';
 export function BarChart (props) {
     
     // -- Task 1.2 -- //
-    const{data, offsetX, offsetY, height, width} = props;    
-    const xScale = Scales.band(data.map(d => `${d.station}`), 0, width);
-    const yScale = Scales.linear(0, max(data, d => d.start), height, 0);
+    const{dataAll, data, offsetX, offsetY, height, width} = props;    
+    const xScale = Scales.band(dataAll.map(d => `${d.station}`), 0, width);
+    const yScale = Scales.linear(0, max(dataAll, d => d.start), height, 0);
     return <g transform={`translate(${offsetX}, ${offsetY})`}>
         <Bars data={data} xScale={xScale} yScale={yScale} height={height}/>
         <YAxis yScale={yScale} height={height} axisLable={"Bikers star from"}/>
--- Q1.2/src/scatterplot.js
+++ Q1.2/src/scatterplot.js
@@ -7,14 +7,14 @@ import { XAxis, YAxis } from "./axes";
 
 export function ScatterPlot(props){
 
-    const {data, offsetX, offsetY, width, height } = props;
-    const xScale = Scales.linear(0, max(data, d => d.tripdurationS), 0, width);
-    const yScale = Scales.linear(0, max(data, d => d.tripdurationE), height, 0);
+    const {dataAll, data, offsetX, offsetY, width, height } = props;
+    const xScale = Scales.linear(0, max(dataAll, d => d.tripdurationS), 0, width);
+    const yScale = Scales.linear(0, max(dataAll, d => d.tripdurationE), height, 0);
     
     return <g transform={`translate(${offsetX}, ${offsetY})`}>
             <Points data={data} xScale={xScale} yScale={yScale} width={width} height={height}/>
-            <YAxis yScale={yScale} height={height} axisLable={"Trip duration start from"}/>
-            <XAxis chartType={"scatter"} xScale={xScale} height={height} width={width} axisLable={"Trip duration end in"} />
+            <YAxis yScale={yScale} height={height} axisLable={"Trip duration end in"}/>
+            <XAxis chartType={"scatter"} xScale={xScale} height={height} width={width} axisLable={"Trip duration start from"} />
         </g>
     
 }
